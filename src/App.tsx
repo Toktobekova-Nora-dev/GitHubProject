031ad7de014f3fc077ae0b0c742908eceb245644
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-02-04 21:09:31 +0300

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Tranding from "./pages/tranding/Tranding";
 import Layout from "./components/layout/Layout";
 
 const App = () => {
   return (
     <div>
-      <Tranding />
       <Layout />
     </div>
   );
